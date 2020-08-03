c24be7c6df21fcba2fb246e9510601a16add9d5f
Alexander Sahli <alexander.m.sahli@gmail.com>
2020-08-03 00:58:09 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -4,7 +4,6 @@ def run_guessing_game
   number = rand(1..6)
   if input == 'exit'
     puts "Goodbye!"
-    break
   elsif input.to_i != number
   puts "Sorry! The computer guessed #{number}"
 elsif input.to_i == number

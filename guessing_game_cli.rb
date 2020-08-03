717058a333aa331ce15a2e86a06671eb9934203b
Alexander Sahli <alexander.m.sahli@gmail.com>
2020-08-03 01:09:22 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -7,7 +7,7 @@ def run_guessing_game
   elsif input.to_i == number
   puts "You guessed the correct number!"
   elsif input.to_i != number
-  puts "Sorry! The computer guessed #{number}"
+  puts "Sorry! The computer guessed #{number}."
 end
 end
 
